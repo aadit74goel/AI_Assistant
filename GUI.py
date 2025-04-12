a8ef8437695d1331b6d7af9817a3a2bbf5f813f7
Harpreet <harpreetchaudhary100@gmail.com>
2025-04-12 12:36:06 +0530

    Added initial imports for PyQt5 GUI

---
+++ GUI.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QStackedWidget, QWidget, QLineEdit, QGridLayout, QVBoxLayout, QHBoxLayout, QPushButton, QFrame, QLabel, QSizePolicy
+from PyQt5.QtGui import QIcon, QPainter, QMovie, QColor, QTextCharFormat, QFont, QPixmap, QTextBlockFormat
+from PyQt5.QtCore import Qt, QSize, QTimer
+from dotenv import dotenv_values
+import sys
+import os
+
+env_vars = dotenv_values(".env")
+Assistantname = env_vars.get("Assistantname")
+current_dir = os.getcwd()
+old_chat_message = ""
+TempDirPath = rf"{current_dir}\Frontend\Files"
+GraphicsDirPath = rf"{current_dir}\Frontend\Graphics"
