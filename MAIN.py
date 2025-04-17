48c8f694b157c24f64c974822fa15b6057829f06
Brijesh <partapbanu3344@gmail.com>
2025-04-17 14:51:20 +0530

    integrate chat log into temporary database file for GUI display

--- MAIN.py
+++ MAIN.py
@@ -8,3 +8,19 @@ from Frontend.GUI import (
     QueryModifier,
     GetMicrophoneStatus,
     GetAssistantStatus)
+
+
+from Backend.Model import FirstLayerDMM
+from Backend.RealtimeSearchEngine import RealtimeSearchEngine
+from Backend.Automation import Automation
+from Backend.SpeechToText import SpeechRecognition
+from Backend.Chatbot import ChatBot
+from Backend.TextToSpeech import TextToSpeech
+from dotenv import dotenv_values
+from asyncio import run
+from time import sleep
+import subprocess
+import sys
+import threading
+import json
+import os
