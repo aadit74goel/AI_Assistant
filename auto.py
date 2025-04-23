170904338a0a5b4878c09897876e8d2a0c2d046d
Aadit <aadit74goel@gmail.com>
2025-04-23 12:20:19 +0530

    initial setup with imports, env loading, and required class selectors

---
+++ auto.py
@@ -0,0 +1,13 @@
+from AppOpener import close, open as appopen
+from webbrowser import open as webopen
+from pywhatkit import search, playonyt
+from dotenv import dotenv_values
+from bs4 import BeautifulSoup
+from rich import print
+from groq import Groq
+import webbrowser
+import subprocess
+import requests
+import keyboard
+import asyncio
+import os
