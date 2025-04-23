5519c4c90732a0eadb4590ded86f9bb9dbc03eb6
Aadit <aadit74goel@gmail.com>
2025-04-23 12:23:15 +0530

    set up Groq client, system prompt, and response templates

--- auto.py
+++ auto.py
@@ -11,3 +11,26 @@ import requests
 import keyboard
 import asyncio
 import os
+
+
+env_vars = dotenv_values(".env")
+GroqAPIKey = env_vars.get("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")
+
+classes = ["zCubwf", "hgKElc", "LTKOO sY7ric", "ZOLcW", "gsrt vk_bk FzvWSb YwPhnf", "pclqee", "tw-Data-text tw-text-small tw-ta", "IZ6rdc", "05uR6d LTKOO", "vlzY6d", "webanswers-webanswers_table_webanswers-table", "dDoNo ikb4Bb gsrt", "sXLa0e",
+"LWkFKe", "VQF4g", "qv3Wpe", "kno-rdesc", "SPZz6b"]
+
+useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"
+
+client = Groq(api_key="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")
+
+professional_responses = [
+    "Your satisfaction is my top priority; feel free to reach out if there's anything else I can help you with.",
+    "I'm at your service for any additional questions or support you may need-don't hesitate to ask.",
+]
+messages = []
+
+SystemChatBot = [{"role": "system", "content": f"Hello, I am {os.environ['Username']}, You're a content writer. You have to write content like letters, codes, applications, essays, notes, songs, poems, etc."}]
+
+def GoogleSearch(Topic):
+    search(Topic)
+    return True
